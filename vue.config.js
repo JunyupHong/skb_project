52c733a9d2b87089092dfee96d127a8797295c6f
JunyupHong <junyup.hong@gmail.com>
2020-07-30 17:36:44 +0900

    [Fix] Promise Modal Component 수정 및 NPM Module 수정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true
+  },
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          $ENV: ${process.env.NODE_ENV};
+        `
+      }
+      // less: {
+      //   javascriptEnabled: true
+      // }
+    }
+  },
+  // "transpileDependencies": [
+  //   "vuetify"
+  // ]
+};
