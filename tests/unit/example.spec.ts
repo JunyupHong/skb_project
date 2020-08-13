fcce531e3b57caa7728f24b71cae561c998a6d7f
JunyupHong <junyup.hong@gmail.com>
2020-08-13 09:03:39 +0900

    [Feat] Unit Test 테스트 중

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,28 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+import PromiseModal from '@/components/promiseModal/promiseModal.vue';
+import Vue from 'vue';
+
+let Constructor;
+let vm: any;
+beforeEach(() => {
+  Constructor = Vue.extend(PromiseModal);
+  vm = new Constructor().$mount();
+});
+
+describe('PromiseModal TEST', () => {
+  it('test1', () => {
+    const component = shallowMount(PromiseModal);
+    expect(component.find('.select-button').trigger('click')).equal(0);
+
+    // const wrapper = shallowMount(PromiseModal, {});
+    // console.log(wrapper);
+    // // expect(wrapper.text()).to.include(msg)
+
+    // expect(msg).to.include('new');
+    // // expect(wrapper.on({}, 'desc', 'TITLE')).to.;
+    // expect(1).equal(1);
+    // // expect(wrapper.on());
+  });
+});
---
+++ tests/unit/promiseModal.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import PromiseModal from '@/components/promiseModal/promiseModal.vue';
+
+describe('promiseModal.vue', () => {
+  // const TodoComponent = mount(App, { propsData: { todo } });
+
+  it('should mount for testing', () => {
+    expect(1).equal(1);
+  });
+});
